c9132806603df24c323b8a10538ec2a77731eb30
n7thz <nathanferreiradev@gmail.com>
2026-01-25 20:15:35 -0300

    feat: inclusão de supabase

---
+++ src/lib/supabase.ts
@@ -0,0 +1,22 @@
+import { createClient } from '@supabase/supabase-js'
+import z from 'zod'
+
+const envSchema = z.object({
+    SUPABASE_URL: z.string().url(),
+    SUPABASE_KEY: z.string().min(1),
+})
+
+const SUPABASE_URL = process.env.SUPABASE_URL
+const SUPABASE_KEY = process.env.SUPABASE_KEY
+
+const { data } = envSchema.safeParse({ SUPABASE_KEY, SUPABASE_URL })
+
+if (!data) {
+    throw new Error('Invalid or missing SUPABASE_URL or SUPABASE_KEY in environment variables')
+}
+
+const { SUPABASE_URL: url, SUPABASE_KEY: key } = data
+
+console.log({ url, key })
+
+export const supabase = createClient(url, key)
