db9fe98895bd0f13ca75ee1fc961860f7fea9926
n7thz <nathanferreiradev@gmail.com>
2026-02-01 23:07:29 -0300

    feat: adição de validação de runtime

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,10 @@
-import { PrismaClient } from "@prisma/client"
-import { PrismaPg } from "@prisma/adapter-pg"
+export const runtime = 'nodejs'
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!,
+})
 
 export const prisma = new PrismaClient({ adapter })
